46a41a02f9448ec212a5e98cf3a5a57b90484f72
notgodfather <ankitranjan10may@gmail.com>
2025-09-07 23:51:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/create-payment-order', async (req, res) => {
+    console.log('Received order:', req.body);
   const { orderId, orderAmount, customerDetails } = req.body;
 
   const clientId = process.env.CASHFREE_CLIENT_ID;
