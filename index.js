45c79635878e49d4916a06e943e60759658764d0
notgodfather <ankitranjan10may@gmail.com>
2025-09-08 02:45:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
