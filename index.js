ea7401f8c3d0064d4b300d6bd60e9d80a771ebbf
notgodfather <ankitranjan10may@gmail.com>
2025-10-12 15:08:43 +0530

    order status by default preparing

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ app.post('/api/record-order', async (req, res) => {
         id: orderId,
         user_id: userId,
         user_email: userEmail,
-        status: 'Pending',
+        status: 'Preparing',
         created_at: new Date().toISOString(),
       }])
       .select('id')
